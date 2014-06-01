c24d0189e0a5cb64f0672e07dbd333f3c5b5f395
“Jaime <jaimedietericho.cr2@gmail.com>
2014-06-01 00:27:39 -0400

    initial commit, gemfile y gitignore modificados

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppInventario::Application.config.secret_token = '1c6d5f98666268ac49aa2cf7d25e75ffd1a14ea49de6ce9c0302e0b386d0f379ca405dae49317c7aba8ea7c6b31b237cf7612fd0e27c69d78d621411e0444e7a'
